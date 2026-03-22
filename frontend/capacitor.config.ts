056c54805ef011fc696239e24adf196a08988da9
Atharva Sharma <2431560atharva@gmail.com>
2026-03-22 23:05:38 +0530

    Fix appId in capacitor.config.ts

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.bondspace.ap',
+    appId: 'com.bondspace.app',
     appName: 'BondSpace',
     webDir: 'out',
     bundledWebRuntime: false,
